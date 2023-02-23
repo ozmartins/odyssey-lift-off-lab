e3573ebc4a260e2e76cf2d723caaf4b6012baabd
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2023-02-23 20:31:46 -0300

    Add module query to schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -6,6 +6,8 @@ const typeDefs = gql`
     tracksForHome: [Track!]!
     "Fetch a specific track, provided a track's ID"
     track(id: ID!): Track!
+    "Fetch a specific module, provided a modules's ID"
+    module(id: ID!): Module!
   }
 
   "A track is a group of Modules that teaches about a specific topic"
@@ -45,6 +47,10 @@ const typeDefs = gql`
     title: String!
     "The module's length in minutes"
     length: Int
+    "The url for module's video"
+    videoUrl: String
+    "The module's content"
+    content: String
   }
 `;
 
